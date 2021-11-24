fa0d7961bdcd0e1ca5e662e0c3dcbe0f74ac9a1e
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-11-24 12:12:35 +0100

    lista y variables

---
+++ RanaLaberinto.py
@@ -0,0 +1,15 @@
+laberinto = [
+    ['I', 'X', 'X', 'B', 'X', 'X'],
+    [' ', 'X', ' ', ' ', ' ', ' '],
+    [' ', 'B', ' ', 'B', 'T2', ' '],
+    [' ', ' ', ' ', 'X', ' ', ' '],
+    [' ', ' ', ' ', 'X', ' ', ' ']
+    ['T2', 'T1', ' ', 'T1', ' ','S']
+]
+
+inicio = 'I'
+salida = 'S'
+bomba = 'B'
+muro = 'X'
+teletransporte_1 = 'T1'
+teletransporte_2 = 'T2'
