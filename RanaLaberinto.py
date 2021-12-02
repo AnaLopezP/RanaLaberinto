c6245ccab9107288fa1b47c32fd15228bbe42150
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-02 12:56:18 +0100

    creacion del laberinto

--- RanaLaberinto.py
+++ RanaLaberinto.py
@@ -13,3 +13,42 @@ bomba = 'B'
 muro = 'X'
 teletransporte_1 = 'T1'
 teletransporte_2 = 'T2'
+
+
+muro = ((0,1), (0,2), (0,4), (0,5), (1,1), (3,3), (4,3))
+bomba = ((0,3),(2,1),(2,3))
+teletransporte_1 = ((5,1), (5,3))
+teletransporte_2 = ((5,0), (2,4)) 
+# maze shape
+#maze1 = [" ","X","X","X","X"]
+#maze2 = [" ","X"," "," "," "]
+#maze3 = [" ","X"," ","X"," "]
+#maze4 = [" "," "," ","X"," "]
+#maze5 = ["X","X","X","X"," "]
+
+
+lab =[]
+def creacionlab():
+        fila = []
+        for i in range(0,6): #line
+                for j in range(0,6): #column
+                                      
+                        if tuple([i,j]) in muro:
+                                fia.append("X")
+                        elif tuple([i,j]) in bomba:
+                                fila.append("B")
+                        elif tuple([i,j]) in teletransporte_1:
+                                fila.append("T1")
+                        elif tuple([i,j]) in teletransporte_2:
+                                fila.append("T2")    
+                        else:
+                                fila.append(" ")
+                lab.append (fila)
+                fila = []
+creacionlab()
+  
+for x in lab:
+    print(" ".join(x))
+
+                
+        
